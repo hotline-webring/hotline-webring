ab05e594c58856ca8ddecd8fbb027cce13ace713
Gabe Berke-Williams <gabebw@gabebw.com>
2018-01-06 17:49:55 -0800

    Don't force SSL on staging
    Staging doesn't have SSL set up.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,6 @@
 require_relative "production"
 
 Rails.application.configure do
+  config.force_ssl = false
   config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
 end
