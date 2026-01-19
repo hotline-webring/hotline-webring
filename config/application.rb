20ec841e5c1a9e87cb052fe289f345bd7bcb45cf
Edward Loveall <edward@edwardloveall.com>
2026-01-19 11:02:45 -0500

    Merge new application config
    I removed our generator options because I can't remember the last time
    we used a generator for anything but a migration.

--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,45 @@
 require_relative "boot"
 
 require "rails"
+# Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module HotlineWebring
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
     config.i18n.enforce_available_locales = true
-    config.assets.quiet = true
-    config.generators do |generate|
-      generate.helper false
-      generate.javascript_engine false
-      generate.request_specs false
-      generate.routing_specs false
-      generate.stylesheets false
-      generate.test_framework :rspec
-      generate.view_specs false
-    end
-    config.action_controller.action_on_unpermitted_parameters = :raise
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.closed = ENV.fetch("CLOSED_FOR_REGISTRATION", false) == "1"
     config.disallow_creating_new_redirections = ENV.fetch("DISALLOW_CREATING_NEW_REDIRECTIONS", false) == "1"
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
