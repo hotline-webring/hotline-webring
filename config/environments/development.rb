d954c043c613ab70a36c0041f6daa457f732ace6
Edward Loveall <edward@edwardloveall.com>
2026-01-19 13:49:20 -0500

    Merge environments wtih rails templates
    Trying to take as much as possible from rails while keeping our Special
    Sauce™

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.cache_classes = false
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Make code changes take effect immediately without server restart.
+  config.enable_reloading = true
+
+  # Do not eager load code on boot.
   config.eager_load = false
+
+  # Show full error reports.
   config.consider_all_requests_local = true
 
   # Tell Rails not to send server timing headers
@@ -13,36 +20,37 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
+    config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
     config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
   else
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
 
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :raise
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
 
+  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-  config.active_record.verbose_query_logs = true
 
-  config.assets.debug = true
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
 
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
-  config.assets.digest = true
-  config.assets.quiet = true
-  config.assets.raise_runtime_errors = true
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
 
+  # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
+
+  # Annotate rendered view with file names.
+  config.action_view.annotate_rendered_view_with_filenames = true
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Disable the concatenation and compression of assets.
+  config.assets.debug = true
 end
