b70c7b961d55f3d8dd7477671e78612cd14bf402
Edward Loveall <edward@edwardloveall.com>
2026-01-19 13:49:20 -0500

    Merge environments wtih rails templates
    Trying to take as much as possible from rails while keeping our Special
    Sauce™

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,32 +1,77 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
-  config.consider_all_requests_local       = false
+
+  # Full error reports are disabled.
+  config.consider_all_requests_local = false
+
+  # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
+
+  # Configures whether Rails should serve static files from the public directory.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.force_ssl = ENV.fetch("USE_SSL", false)
+
+  # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
-  config.middleware.use Rack::Deflater
-  config.middleware.use Rack::CanonicalHost, ENV.fetch("APPLICATION_HOST")
-  config.assets.compile = false
-  config.assets.digest = true
-  config.log_level = :debug
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
-  config.i18n.fallbacks = true
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
+
+  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
+  # config.assume_ssl = true
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = ENV.fetch("USE_SSL", false)
+
+  # Skip http-to-https redirect for the default health check endpoint.
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+
+  # Log to STDOUT with the current request id as a default log tag.
+  config.log_tags = [ :request_id ]
+  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+
+  # Change to "debug" to log everything (including potentially personally-identifiable information!).
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+
+  # Prevent health checks from clogging up the logs.
+  config.silence_healthcheck_path = "/up"
+
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
-  config.log_formatter = ::Logger::Formatter.new
+
+  # Replace the default in-process memory cache store with a durable alternative.
+  # config.cache_store = :mem_cache_store
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
+
+  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
   # Show all attributes when inspecting objects in production.
   # (The default in Rails 8 is [:id], but showing an object isn't slow in our
   # app, so we can look at every attribute.)
   config.active_record.attributes_for_inspect = :all
-  config.log_tags = [:request_id]
+
+  # Enable DNS rebinding protection and other `Host` header attacks.
+  # config.hosts = [
+  #   "example.com",     # Allow requests from example.com
+  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  # ]
+  #
+  # Skip DNS rebinding protection for the default health check endpoint.
+  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.middleware.use Rack::Deflater
+  config.middleware.use Rack::CanonicalHost, ENV.fetch("APPLICATION_HOST")
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
